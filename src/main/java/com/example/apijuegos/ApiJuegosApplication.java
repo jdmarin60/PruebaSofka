72507b7843abd2b5c1fb15e1e33fb8a263d80cdc
jdmarin60 <jdmarin60@gmail.com>
2022-04-25 10:15:34 -0500

    Initial Commit

---
+++ src/main/java/com/example/apijuegos/ApiJuegosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apijuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJuegosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiJuegosApplication.class, args);
+    }
+
+}
