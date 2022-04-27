462fec12a1d3588c6716f8efe3ebab9c810ea58e
jdmarin60 <jdmarin60@gmail.com>
2022-04-27 03:21:25 -0500

    Updating imports PremioModel

--- src/main/java/com/example/apijuegos/Premio/Dominio/PremioModel.java
+++ src/main/java/com/example/apijuegos/Premio/Dominio/PremioModel.java
@@ -4,11 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import javax.persistence.Column;
-import java.sql.Timestamp;
 
 @AllArgsConstructor
 @NoArgsConstructor
